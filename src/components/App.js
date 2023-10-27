8be2f4e8c1e1256814d30ab45f28cb73b98b469b
DidasMba <daniel.mbarushi@gmail.com>
2023-10-28 00:29:20 +0300

    props desruction

--- src/components/App.js
+++ src/components/App.js
@@ -16,13 +16,4 @@ function App() {
   );
 }
 
-// child component
-function MovieCard(props) {
-  return (
-    <div className="movie-card">
-      <img src={props.posterSrc} alt={props.title} />
-      <h2>{props.title}</h2>
-      <small>{props.genres.join(", ")}</small>
-    </div>
-  );
-}
+export default App;
